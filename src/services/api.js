ee45575b90f015e8d3bf6379f793d1737f819e73
Sonali Nachankaar <80123687+sonali168@users.noreply.github.com>
2024-11-24 20:17:38 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://socailmedia-backend-rl1h.onrender.com',  
+  baseURL: 'https://socailmedia-backend-j2oa.onrender.com/',  
 });
 
 API.interceptors.request.use((req) => {
