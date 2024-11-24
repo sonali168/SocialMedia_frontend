df748a531e6fb7e1cf980f3caa2d74cea3aa017a
Sonali Nachankaar <80123687+sonali168@users.noreply.github.com>
2024-11-24 19:47:25 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-// Create an Axios instance with the base URL of your backend
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',  // Set the base URL for all requests
+  baseURL: 'https://socailmedia-backend-rl1h.onrender.com',  
 });
 
-// Attach JWT token (if available) to every request
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
   if (token) {
